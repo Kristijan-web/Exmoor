076c8d161389b43eaae55833cc88178e56bc52c9
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-05 13:54:00 +0100

    finished grid layout for aboutProducts section

--- src/features/HomeDisplay/Sections/AboutProducts/AboutProducts.jsx
+++ src/features/HomeDisplay/Sections/AboutProducts/AboutProducts.jsx
@@ -1,14 +1,10 @@
 export default function AboutProducts() {
   return (
-    <section className="h-[100vh] w-full">
-      <div className="grid h-[100vh] grid-cols-12 grid-rows-2">
-        <div className="col-start-1 col-end-6 row-span-full bg-[url(/Images/AboutProducts/treci_1.webp)] bg-no-repeat">
-          Test
-        </div>
-        <div className="col-start-6 col-end-12 bg-[url(/Images/AboutProducts/cetvrti_1.webp)] bg-contain bg-no-repeat">
-          Test
-        </div>
-        <div className="col-start-6 col-end-12 row-span-2">Test</div>
+    <section className="h-[110vh] w-full">
+      <div className="grid h-[110vh] grid-cols-12 grid-rows-2 gap-[1px]">
+        <div className="col-start-1 col-end-7 row-span-full bg-[url(/Images/AboutProducts/teest2.jpg)] bg-cover bg-no-repeat"></div>
+        <div className="col-start-7 col-end-13 bg-[url(/Images/AboutProducts/teest.jpg)] bg-cover bg-no-repeat"></div>
+        <div className="col-start-7 col-end-13 row-span-2 bg-[url(/Images/AboutProducts/peti_1.webp)] bg-cover bg-center bg-no-repeat"></div>
       </div>
     </section>
   );
