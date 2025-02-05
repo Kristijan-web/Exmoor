6eb4502d88030d75d50fc97d7400ce215d032723
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-05 13:11:15 +0100

    preparing to change the grid layout

--- src/features/HomeDisplay/Sections/AboutProducts/AboutProducts.jsx
+++ src/features/HomeDisplay/Sections/AboutProducts/AboutProducts.jsx
@@ -1,9 +1,13 @@
 export default function AboutProducts() {
   return (
-    <section className="h-[80vh] w-full">
-      <div className="grid grid-cols-12 grid-rows-2">
-        <div className="col-start-1 col-end-6 row-span-full">Test</div>
-        <div className="col-start-6 col-end-12">Test</div>
+    <section className="h-[100vh] w-full">
+      <div className="grid h-[100vh] grid-cols-12 grid-rows-2">
+        <div className="col-start-1 col-end-6 row-span-full bg-[url(/Images/AboutProducts/treci_1.webp)] bg-no-repeat">
+          Test
+        </div>
+        <div className="col-start-6 col-end-12 bg-[url(/Images/AboutProducts/cetvrti_1.webp)] bg-contain bg-no-repeat">
+          Test
+        </div>
         <div className="col-start-6 col-end-12 row-span-2">Test</div>
       </div>
     </section>
