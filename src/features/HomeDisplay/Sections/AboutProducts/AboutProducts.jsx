a77384710d1b653b1f3ec3a89c6707af7cd31025
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-08 23:15:09 +0100

    finished about products sections and made it responsive

--- src/features/HomeDisplay/Sections/AboutProducts/AboutProducts.jsx
+++ src/features/HomeDisplay/Sections/AboutProducts/AboutProducts.jsx
@@ -1,19 +1,21 @@
 export default function AboutProducts() {
   return (
     <section className="h-[110vh] w-full">
-      <div className="grid h-[110vh] grid-cols-12 grid-rows-2 gap-[1px]">
-        <div className="gradient_image_man col-start-1 col-end-7 row-span-full flex items-end justify-center bg-cover bg-no-repeat p-7">
-          <span className="text-secondary-color text-4xl">Man fragrances</span>
+      <div className="grid h-[110vh] grid-cols-12 grid-rows-3 gap-[1px] sm:grid-rows-2">
+        <div className="gradient_image_man col-start-1 col-end-13 flex items-end justify-center bg-cover bg-center bg-no-repeat pb-7 sm:col-start-1 sm:col-end-7 sm:row-span-full sm:bg-left">
+          <span className="text-secondary-color text-center text-4xl">
+            Man fragrances
+          </span>
         </div>
 
-        <div className="gradient_image_woman col-start-7 col-end-13 flex items-end justify-center bg-cover bg-no-repeat p-7">
-          <span className="text-secondary-color text-4xl">
+        <div className="gradient_image_woman col-start-1 col-end-13 flex items-end justify-center bg-cover bg-no-repeat pb-7 sm:col-start-7 sm:col-end-13">
+          <span className="text-secondary-color text-center text-4xl">
             Woman fragrances
           </span>
         </div>
-        <div className="gradient_image_home col-start-7 col-end-13 row-span-2 flex items-end justify-center bg-cover bg-center bg-no-repeat p-7">
-          <span className="text-secondary-color text-4xl">
-            Woman fragrances
+        <div className="gradient_image_home col-start-1 col-end-13 flex items-end justify-center bg-cover bg-center bg-no-repeat pb-7 sm:col-start-7 sm:col-end-13 sm:row-span-2">
+          <span className="text-secondary-color text-center text-4xl">
+            Home fragrances
           </span>
         </div>
       </div>
