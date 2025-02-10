5f073cdd3199c8e13a92b029b36b2ba8ca48ad64
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-10 22:49:16 +0100

    made reviewers section responsive for mobile, now for laptop and desktop

--- src/features/HomeDisplay/Sections/Reviews/Reviews.jsx
+++ src/features/HomeDisplay/Sections/Reviews/Reviews.jsx
@@ -1,7 +1,7 @@
 export default function Reviews() {
   return (
     <section>
-      <div className="mb-10 grid grid-cols-12">
+      <div className="mb-20 grid grid-cols-12 px-7">
         <div className="bg-first-review col-start-1 col-end-4 flex h-24 items-center justify-center">
           <h3 className="text-8xl">Reviews</h3>
         </div>
@@ -9,10 +9,59 @@ export default function Reviews() {
         <div className="bg-main-color-shade col-start-8 col-end-13 h-24"></div>
       </div>
       {/* ovo ispod su kartice za reviewers */}
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="grid grid-cols-1 gap-20 px-7">
+        <div className="relative flex flex-col items-center justify-center gap-8 bg-orange-500/50 p-9">
+          <p>
+            Awesome fragrance for man, it lasts long, smells good and it does
+            not dissapoint! Would definitely recommend!
+          </p>
+          <b>Rodrigo</b>
+
+          <img
+            className="w-40"
+            src="Images/Reviewers/five-star.png"
+            alt="star_rating"
+          />
+          <img
+            className="absolute top-[-60px] left-[50%] w-20 translate-x-[-50%] transform rounded-full"
+            src="/Images/Reviewers/model-1.png"
+            alt="reviewer_image"
+          />
+        </div>
+        <div className="relative flex flex-col items-center justify-center gap-8 bg-yellow-500/50 p-9">
+          <p>
+            Awesome fragrance for man, it lasts long, smells good and it does
+            not dissapoint! Would definitely recommend!
+          </p>
+          <b>Rodrigo</b>
+          <img
+            className="w-40"
+            src="Images/Reviewers/five-star.png"
+            alt="star_rating"
+          />
+          <img
+            className="absolute top-[-60px] left-[50%] w-20 translate-x-[-50%] transform rounded-full"
+            src="/Images/Reviewers/model-2.png"
+            alt="reviewer_image"
+          />
+        </div>
+        <div className="relative flex flex-col items-center justify-center gap-8 bg-purple-500/50 p-9">
+          <p>
+            Awesome fragrance for man, it lasts long, smells good and it does
+            not dissapoint! Would definitely recommend!
+          </p>
+          <b>Rodrigo</b>
+          <img
+            className="w-40"
+            src="Images/Reviewers/five-star.png"
+            alt="star_rating"
+          />
+          <img
+            className="absolute top-[-60px] left-[50%] w-20 translate-x-[-50%] transform rounded-full"
+            src="/Images/Reviewers/model-3.png"
+            alt="reviewer_image"
+          />
+        </div>
       </div>
     </section>
   );
