66f75350d88aa8be00c35f03ef9920779c6a08ae
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-11 18:52:18 +0100

    almost finished reviews section, made it responsive, just need to make few adjustments

--- src/features/HomeDisplay/Sections/Reviews/Reviews.jsx
+++ src/features/HomeDisplay/Sections/Reviews/Reviews.jsx
@@ -1,7 +1,7 @@
 export default function Reviews() {
   return (
     <section>
-      <div className="mb-20 grid grid-cols-12 px-7">
+      <div className="mb-40 grid grid-cols-12 px-7">
         <div className="bg-first-review col-start-1 col-end-4 flex h-24 items-center justify-center">
           <h3 className="text-8xl">Reviews</h3>
         </div>
@@ -9,14 +9,13 @@ export default function Reviews() {
         <div className="bg-main-color-shade col-start-8 col-end-13 h-24"></div>
       </div>
       {/* ovo ispod su kartice za reviewers */}
-      <div className="grid grid-cols-1 gap-20 px-7">
+      <div className="grid grid-cols-1 gap-20 px-7 md:grid-cols-3 xl:px-24">
         <div className="relative flex flex-col items-center justify-center gap-8 bg-orange-500/50 p-9">
-          <p>
+          <p className="mt-8 text-center lg:text-start">
             Awesome fragrance for man, it lasts long, smells good and it does
             not dissapoint! Would definitely recommend!
           </p>
           <b>Rodrigo</b>
-
           <img
             className="w-40"
             src="Images/Reviewers/five-star.png"
@@ -29,9 +28,9 @@ export default function Reviews() {
           />
         </div>
         <div className="relative flex flex-col items-center justify-center gap-8 bg-yellow-500/50 p-9">
-          <p>
-            Awesome fragrance for man, it lasts long, smells good and it does
-            not dissapoint! Would definitely recommend!
+          <p className="mt-8 text-center lg:text-start">
+            Was really suprised, one of the best fragrances i have ever bought.
+            Just ordered one for my friend. #ExmoorFan
           </p>
           <b>Rodrigo</b>
           <img
@@ -46,9 +45,9 @@ export default function Reviews() {
           />
         </div>
         <div className="relative flex flex-col items-center justify-center gap-8 bg-purple-500/50 p-9">
-          <p>
-            Awesome fragrance for man, it lasts long, smells good and it does
-            not dissapoint! Would definitely recommend!
+          <p className="mt-8 text-center lg:text-start">
+            Heard about Exmoor from my friend, one of his best advices ever.
+            Exmoor is the only fragrance for me.
           </p>
           <b>Rodrigo</b>
           <img
