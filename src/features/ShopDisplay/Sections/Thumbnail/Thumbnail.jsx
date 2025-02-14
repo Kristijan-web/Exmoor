485b45a4ae87d0f5ea224da94b129eb3f5fb92cb
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-14 21:28:42 +0100

    Finished thumbnail on shop page and made it reponsive

--- src/features/ShopDisplay/Sections/Thumbnail/Thumbnail.jsx
+++ src/features/ShopDisplay/Sections/Thumbnail/Thumbnail.jsx
@@ -2,7 +2,7 @@ export default function Thumbnail() {
   return (
     <section className="h-thumbnail gradient_image_shop bg-start relative w-full bg-cover bg-no-repeat sm:bg-center">
       <div className="text-secondary-color absolute top-[50%] mx-auto max-w-7xl translate-y-[-50%] px-7 sm:left-[10%]">
-        <div className="flex w-full flex-col gap-3 sm:w-[70%]">
+        <div className="flex w-full flex-col gap-4 sm:w-[70%] sm:gap-3">
           <h1 className="font-semi text-start text-4xl">
             Choose from one of the best perfume sellers in the world
           </h1>
