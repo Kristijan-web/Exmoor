c768387595e8046cee55a8943a4ec0e1d7a8f60a
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-02-19 19:19:35 +0100

    Almost finished making layout for filter and sorting of producits

--- src/features/ShopDisplay/Sections/Products&Filters/Filters/Filters.jsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Filters/Filters.jsx
@@ -1,11 +1,29 @@
 export default function Filters() {
   return (
-    <div className="cursor-poiner bg-main-color-shade text-secondary-color col-start-1 col-end-7 rounded-sm py-4">
-      <div className="flex items-center justify-between px-4">
+    <div className="cursor-poiner bg-main-color-shade text-secondary-color lg:text-main-color-shade col-start-1 col-end-7 rounded-sm py-4 lg:col-start-1 lg:col-end-4 lg:row-start-1 lg:row-end-3 lg:h-full lg:border-1 lg:border-black lg:bg-white lg:p-5 lg:py-4">
+      <div className="flex items-center justify-between px-4 lg:hidden">
         <span>Filters</span>
         <span>
           <ion-icon name="chevron-down-outline"></ion-icon>
         </span>
+      </div>
+      <div className="hidden lg:block">
+        <h3 className="mb-6 text-center text-xl">Filtriraj parfeme</h3>
+        <div>
+          <p className="mb-3">Proizvodjac</p>
+          <div>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-4">
+                <input type="checkbox" />
+                <span>Zara</span>
+              </div>
+
+              <span>15</span>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/features/ShopDisplay/Sections/Products&Filters/Sort/Sort.jsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Sort/Sort.jsx
@@ -1,11 +1,14 @@
 export default function Sort() {
   return (
-    <div className="cursor-poiner bg-main-color-shade text-secondary-color col-start-7 col-end-13 rounded-sm py-4">
-      <div className="flex items-center justify-between px-4">
+    <div className="lg:text-main-color-shade cursor-poiner bg-main-color-shade text-secondary-color col-start-7 col-end-13 rounded-sm py-4 lg:col-start-4 lg:col-end-13 lg:border-1 lg:border-black lg:bg-white">
+      <div className="flex items-center justify-between px-4 lg:hidden">
         <span>Sort</span>
         <span>
           <ion-icon name="chevron-down-outline"></ion-icon>
         </span>
+      </div>
+      <div className="hidden px-5 lg:block">
+        <p className="text-end text-xl">Sortiraj</p>
       </div>
     </div>
   );
