5653d3e97525d68313f613fa8f9ba16a6b20e06c
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-03-06 02:05:30 +0100

    finished default user settings in settings page

--- src/features/SettingsDisplay/OtherSettings/OtherSettings.tsx
+++ src/features/SettingsDisplay/OtherSettings/OtherSettings.tsx
@@ -2,50 +2,65 @@ export default function OtherSettings() {
   return (
     <section className="col-start-4 col-end-15 mt-15">
       <div className="mx-auto max-w-4xl px-7">
-        <h2 className="mb-15">Vas nalog</h2>
         <form className="bg-secondary-new flex flex-col items-start justify-start gap-5 p-12">
           <h3 className="mb-10">Vase postavke</h3>
-          <div className="flex gap-5">
-            <div className="flex w-110 items-center justify-between">
-              <label htmlFor="fullName">Ime i prezime</label>
+          <div className="grid w-full grid-cols-2 gap-15">
+            <div className="items-star flex flex-col gap-2">
+              <label htmlFor="username">Ime i prezime</label>
               <input
-                id="fullName"
+                id="username"
                 type="text"
-                className="border-main-color-shade h-10 rounded-xs border-1 bg-white p-3"
+                className="h-10 rounded-xs border-none bg-white p-3"
               />
             </div>
-            <div>
+            <div className="items-star flex flex-col gap-2">
               <label htmlFor="email">Email</label>
               <input
                 id="email"
-                type="email"
-                className="rounded-xs border-1 border-black"
+                type="text"
+                className="h-10 rounded-xs border-none bg-white p-3"
               />
             </div>
-          </div>
-          <div className="flex w-110 items-center justify-between">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              className="rounded-xs border-1 border-black"
-            />
-          </div>
-          <div className="mb-10 flex w-110 items-center justify-between">
-            <label htmlFor="number">Telefon</label>
-            <input
-              id="number"
-              type="number"
-              className="rounded-xs border-1 border-black"
-            />
-          </div>
-          <div className="flex w-110 items-center justify-end gap-5">
-            <button className="btn flex h-10 w-25 items-center justify-center">
-              Odustani
-            </button>
-            <button className="btn flex h-10 w-25 items-center justify-center">
-              Potvrdi
-            </button>
+            <div className="items-star flex flex-col gap-2">
+              <label htmlFor="city">Grad</label>
+              <input
+                id="city"
+                type="text"
+                className="h-10 rounded-xs border-none bg-white p-3"
+              />
+            </div>
+            <div className="items-star flex flex-col gap-2">
+              <label htmlFor="postalCode">Postanski broj</label>
+              <input
+                id="postalCode"
+                type="text"
+                className="h-10 rounded-xs border-none bg-white p-3"
+              />
+            </div>
+            <div className="items-star flex flex-col gap-2">
+              <label htmlFor="address">Adresa</label>
+              <input
+                id="address"
+                type="text"
+                className="h-10 rounded-xs border-none bg-white p-3"
+              />
+            </div>
+            <div className="items-star flex flex-col gap-2">
+              <label htmlFor="phone">Broj telefona</label>
+              <input
+                id="phone"
+                type="text"
+                className="h-10 rounded-xs border-none bg-white p-3"
+              />
+            </div>
+            <div className="flex items-center justify-start gap-5 text-center">
+              <button className="btn flex h-10 w-25 items-center justify-center">
+                Odustani
+              </button>
+              <button className="btn flex h-10 w-25 items-center justify-center">
+                Potvrdi
+              </button>
+            </div>
           </div>
         </form>
       </div>
