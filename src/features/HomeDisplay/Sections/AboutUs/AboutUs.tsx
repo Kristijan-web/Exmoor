6404c3047444bdedff0fbe193fae6699c652ca47
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-03-08 22:25:28 +0100

    changed color of background and changed css so image shows at 1280px(xl tailwind)

--- src/features/HomeDisplay/Sections/AboutUs/AboutUs.tsx
+++ src/features/HomeDisplay/Sections/AboutUs/AboutUs.tsx
@@ -1,10 +1,10 @@
 export default function AboutUs() {
   return (
-    <section className="bg-secondary-color mx-auto mb-24 max-w-6xl rounded-sm pt-10 pb-10 lg:pb-2">
-      <div className="flex w-full items-start gap-20 px-4 lg:px-8">
-        <div className="w-full lg:w-[50%]">
-          <h3 className="mb-8 text-center lg:text-start">About us</h3>
-          <p className="mb-4 text-center text-xl tracking-wide lg:text-start">
+    <section className="bg-secondary-new mb-24 h-120 xl:pb-2">
+      <div className="mx-auto flex h-full w-full max-w-6xl items-center gap-20 px-7 xl:px-7">
+        <div className="w-full xl:w-[50%]">
+          <h3 className="mb-8 text-center xl:text-start">About us</h3>
+          <p className="mb-4 text-center text-xl tracking-wide xl:text-start">
             Welcome to Exmoor, your premier destination for luxury fragrances.
             At Exmoor, we offer an exquisite selection of perfumes from
             world-renowned brands like Dior, Calvin Klein, and many others. Our
@@ -12,11 +12,11 @@ export default function AboutUs() {
             bringing you the most iconic scents that elevate your presence and
             leave a lasting impression.
           </p>
-          <div className="text-center lg:text-start">
+          <div className="text-center xl:text-start">
             <button className="btn">Products</button>
           </div>
         </div>
-        <div className="hidden lg:flex lg:w-[50%] lg:justify-center">
+        <div className="hidden xl:flex xl:w-[50%] xl:justify-center">
           <img
             className="inline-block w-[72%] rounded-full"
             src="/Images/HomePage/AboutUs/about-us.webp"
