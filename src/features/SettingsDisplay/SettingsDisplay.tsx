414cf4671b63eca7b4766db629ce89fed27e5122
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-03-17 20:23:54 +0100

    started doing responsive on new design

--- src/features/SettingsDisplay/SettingsDisplay.tsx
+++ src/features/SettingsDisplay/SettingsDisplay.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
-// import PasswordDisplay from "./PasswordSetting/PasswordSetting";
 import SettingsMenuSidebar from "./SettingsMenuSidebar/SettingsMenuSidebar";
 
 export default function SettingsDisplay() {
   return (
     <div className="mx-auto grid max-w-6xl grid-cols-1 lg:grid-cols-[305px_1fr] lg:p-7">
       <SettingsMenuSidebar />
-      <Outlet />
+      <section className="hidden border-1 border-l-0 border-black lg:block">
+        <div className="mx-auto h-full sm:p-7">
+          <Outlet />
+        </div>
+      </section>
     </div>
   );
 }
