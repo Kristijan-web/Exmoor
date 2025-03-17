e13d55ad734ed94e5ab66a56f4bc0a0222f76f28
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-03-17 21:09:47 +0100

    refactored a bit of code to follow a convetion

--- src/features/SettingsDisplay/SettingsDisplay.tsx
+++ src/features/SettingsDisplay/SettingsDisplay.tsx
@@ -2,16 +2,12 @@ import { Outlet } from "react-router-dom";
 import SettingsMenuSidebar from "./SettingsMenuSidebar/SettingsMenuSidebar";
 
 export default function SettingsDisplay() {
-  const [showUserSettings, setShowUserSettings] = useState(false);
+  // const [showUserSettings, setShowUserSettings] = useState(false);
 
   return (
     <div className="mx-auto grid max-w-6xl grid-cols-1 lg:grid-cols-[305px_1fr] lg:p-7">
       <SettingsMenuSidebar />
-      <section className="hidden border-1 border-l-0 border-black lg:block">
-        <div className="mx-auto h-full sm:p-7">
-          <Outlet />
-        </div>
-      </section>
+      <Outlet />
       {/* PA MOGLO JE OVAKO */}
       {/* showUserSettings && <Outlet/> */}
     </div>
--- src/features/SettingsDisplay/UserSettings/PasswordSetting.tsx
+++ src/features/SettingsDisplay/UserSettings/PasswordSetting.tsx
@@ -1,49 +1,51 @@
 export default function PasswordSetting() {
   return (
-    <div className="flex justify-center">
-      <form className="flex flex-col items-start justify-start gap-5 p-12">
-        <h3 className="mb-10 text-start">Promena šifre</h3>
-        <div className="flex flex-col gap-5">
-          <div className="flex flex-col gap-2">
-            <label htmlFor="old-password " className="text-xl">
-              Stara šifra
-            </label>
-            <input
-              type="text"
-              id="old-password"
-              placeholder="Stara šifra"
-              className="w-70 rounded-xs border-1 border-black p-1"
-            />
+    <section className="hidden border-1 border-l-0 border-black lg:block">
+      <div className="mx-auto flex h-full justify-center sm:p-7">
+        <form className="flex flex-col items-start justify-start gap-5 p-12">
+          <h3 className="mb-10 text-start">Promena šifre</h3>
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="old-password " className="text-xl">
+                Stara šifra
+              </label>
+              <input
+                type="text"
+                id="old-password"
+                placeholder="Stara šifra"
+                className="w-70 rounded-xs border-1 border-black p-1"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="old-password" className="text-xl">
+                Nova šifra
+              </label>
+              <input
+                type="text"
+                id="old-password"
+                placeholder="Nova šifra"
+                className="w-70 rounded-xs border-1 border-black p-1"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="old-password" className="text-xl">
+                Ponovi novu šifru
+              </label>
+              <input
+                type="text"
+                id="old-password"
+                placeholder="Ponovi novu šifru"
+                className="w-70 rounded-xs border-1 border-black p-1"
+              />
+            </div>
+            <div>
+              <button className="btn mt-4 flex h-10 w-25 items-center justify-center">
+                Potvrdi
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="old-password" className="text-xl">
-              Nova šifra
-            </label>
-            <input
-              type="text"
-              id="old-password"
-              placeholder="Nova šifra"
-              className="w-70 rounded-xs border-1 border-black p-1"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="old-password" className="text-xl">
-              Ponovi novu šifru
-            </label>
-            <input
-              type="text"
-              id="old-password"
-              placeholder="Ponovi novu šifru"
-              className="w-70 rounded-xs border-1 border-black p-1"
-            />
-          </div>
-          <div>
-            <button className="btn mt-4 flex h-10 w-25 items-center justify-center">
-              Potvrdi
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </section>
   );
 }
