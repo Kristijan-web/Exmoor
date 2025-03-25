13f088ab5be424eb03c3e56652882f3b07812d89
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-03-25 19:12:21 +0100

    Finished responsive design for purchased product setting in settings page

--- src/features/SettingsDisplay/UserSettings/PurchasedProducts/PurchasedProduct/Product.tsx
+++ src/features/SettingsDisplay/UserSettings/PurchasedProducts/PurchasedProduct/Product.tsx
@@ -1,14 +1,14 @@
 export default function Product() {
   return (
-    <div className="bg-secondary-new flex items-center justify-between gap-5 border-1 border-black p-4">
+    <div className="bg-secondary-new flex flex-col items-center justify-between gap-5 border-1 border-black p-4 sm:flex-row">
       <div className="flex flex-col items-center gap-5 sm:flex-row">
-        <div className="w-[80%] sm:w-[15%]">
+        <div className="w-[git 0%] sm:w-[15%]">
           <img
             className="w-full"
             src="/public/Images/ShopPage/Products/shop-item-1.jpg"
           />
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col items-center gap-3 sm:items-start">
           <p>Zara Light Touch</p>
           <span>Muski</span>
           <span>3.999 RSD</span>
--- src/features/SettingsDisplay/UserSettings/PurchasedProducts/PurchasedProducts.tsx
+++ src/features/SettingsDisplay/UserSettings/PurchasedProducts/PurchasedProducts.tsx
@@ -3,7 +3,7 @@ import Product from "./PurchasedProduct/Product";
 export default function PurchasedProducts() {
   return (
     <div className="mx-auto flex h-full flex-col p-7">
-      <div className="flex items-center justify-center p-12">
+      <div className="mt-12 flex items-center justify-center">
         <h3 className="mb-10">Kupljeni proizvodi</h3>
       </div>
       <div>
