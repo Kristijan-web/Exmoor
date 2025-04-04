49e648554a76885d7dfe9a392ed797a3920af375
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-04 23:59:44 +0200

    Triyng to make IDE recomend necessary imports

--- src/features/CartDisplay/CloseCart/__test__/CloseCart.test.tsx
+++ src/features/CartDisplay/CloseCart/__test__/CloseCart.test.tsx
@@ -1,2 +1,12 @@
 import CloseCart from "../CloseCart";
-test;
+import { render, screen } from "@testing-library/react";
+import { it, expectgit } from "vitest";
+import "@testing-library/jest-dom";
+
+it("expected to display 'Hello, Pedro'", () => {
+  render(<CloseCart />);
+  const display = screen.getByText("Hello, Pedro!");
+  expect(display).toBeInTheDocument();
+});
+
+expe;
