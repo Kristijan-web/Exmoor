65188ffb76e7c6ebf72d0933cb0fb7603c58a7d7
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-05 23:46:36 +0200

    Learning to write tests using vitest and react testing library

--- src/features/CartDisplay/CloseCart/__test__/CloseCart.test.tsx
+++ src/features/CartDisplay/CloseCart/__test__/CloseCart.test.tsx
@@ -1,12 +1,20 @@
-import CloseCart from "../CloseCart";
-import { render, screen } from "@testing-library/react";
-import { it, expectgit } from "vitest";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { expect, test } from "vitest";
 import "@testing-library/jest-dom";
+import CartProvider from "../../../../contexts/GlobalContexts/CartContext";
+import CloseCart from "../CloseCart";
 
-it("expected to display 'Hello, Pedro'", () => {
-  render(<CloseCart />);
-  const display = screen.getByText("Hello, Pedro!");
-  expect(display).toBeInTheDocument();
+test("Is cart closing when dispatch for context is being called", async () => {
+  render(
+    <CartProvider>
+      <CloseCart />
+    </CartProvider>,
+  );
+  const closeButton = screen.getByText("✖");
+
+  fireEvent.click(closeButton);
+  // mora waitFor jer bilo kakav rad sa context-om se desava asinhrono
+  await waitFor(() => {
+    expect(screen.queryByText("✖")).not.toBeInTheDocument();
+  });
 });
-
-expe;
