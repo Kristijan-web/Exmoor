cbf1436c1f0cc7c4913fb1b0dab9b488a3071f8d
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-12 23:20:55 +0200

    Installed react-query and react-query-devtools and connected them to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
+
   return (
     <>
-      <Toaster
-        position="top-center"
-        gutter={12}
-        toastOptions={{
-          success: { duration: 3000 },
-          error: { duration: 3000 },
-          style: { fontSize: "16px" },
-        }}
-      />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Toaster
+          position="top-center"
+          gutter={12}
+          toastOptions={{
+            success: { duration: 3000 },
+            error: { duration: 3000 },
+            style: { fontSize: "16px" },
+          }}
+        />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 }
--- src/services/Supabase.ts
+++ src/services/Supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://uaeozjttrvrpjlgyopnu.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhZW96anR0cnZycGpsZ3lvcG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1NDQzMTgsImV4cCI6MjA0NTEyMDMxOH0.yyDHe018Kx7fLRtKnlEtpONkDo9Ou6VGfIRD6YSoHIQ`;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
