40a03a696eda8e9f26f01efc74e91890a83ab022
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-17 00:19:23 +0200

    made spinner display in shop page when fetching products and made it responsive

--- src/features/ShopDisplay/Sections/Products&Filters/Products/Products.tsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Products/Products.tsx
@@ -1,40 +1,32 @@
 import toast from "react-hot-toast";
 import useProducts from "../../../../../hooks/APIHooks/useProducts";
 import ProductItem from "./ProductItem";
-
+// grid grid-cols-1 justify-self-center sm:grid-cols-2 md:grid-cols-3 lg:col-start-4 lg:col-end-14 lg:mt-0
 export default function Products() {
   const { products, isLoading, error } = useProducts();
-  if (isLoading)
+  if (true)
     return (
-      <div>
-        <span style={{ 
-            width: '48px',
-    height: '48px',
-    border: '5px solid #FFF',
-    borderBottomColor:' transparent',
-    borderRadius: '50%',
-    display:' inline-block',
-    box-sizing:' border-box',
-    animation: 'rotation 1s linear infinite'}}></span>
+      <div className="col-span-full text-center lg:col-start-4 lg:col-end-13">
+        <span className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-black border-b-transparent lg:h-16 lg:w-16"></span>
       </div>
     );
   if (error) {
     toast.error("Error! Please reload the page");
   }
-  return (
-    <div className="col-span-full mt-8 grid grid-cols-1 gap-5 justify-self-center sm:grid-cols-2 md:grid-cols-3 lg:col-start-4 lg:col-end-13 lg:mt-0 lg:grid-cols-3 2xl:grid-cols-4">
-      {products?.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
-      {/* <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem /> */}
-    </div>
-  );
+  // return (
+  //   <div className="col-span-full mt-8 grid grid-cols-1 gap-5 justify-self-center sm:grid-cols-2 md:grid-cols-3 lg:col-start-4 lg:col-end-13 lg:mt-0 lg:grid-cols-3 2xl:grid-cols-4">
+  //     {products?.map((product) => (
+  //       <ProductItem key={product.id} product={product} />
+  //     ))}
+  //     {/* <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem />
+  //     <ProductItem /> */}
+  //   </div>
+  // );
 }
