0d5700d23e8d24335cca4f4e1728ba9b8c3348e8
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-17 23:36:03 +0200

    connected supabase database to frontend

--- src/services/Supabase.ts
+++ src/services/Supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://uaeozjttrvrpjlgyopnu.supabase.co";
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhZW96anR0cnZycGpsZ3lvcG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1NDQzMTgsImV4cCI6MjA0NTEyMDMxOH0.yyDHe018Kx7fLRtKnlEtpONkDo9Ou6VGfIRD6YSoHIQ`;
+const supabaseUrl = "https://ksdtxkabydxpsyogdbdb.supabase.co";
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZHR4a2FieWR4cHN5b2dkYmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NzI0NjksImV4cCI6MjA2MDE0ODQ2OX0.T3-cleAU5dDycOcwgKT9zxe1Pa5BD3OE4jRccMo7TW0`;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
