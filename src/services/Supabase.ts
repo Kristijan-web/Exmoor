e30410a58d8d96c338b299bd00e8dea86ebe0b5a
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-18 11:53:27 +0200

    reading key from env file

--- src/services/Supabase.ts
+++ src/services/Supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://ksdtxkabydxpsyogdbdb.supabase.co";
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZHR4a2FieWR4cHN5b2dkYmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NzI0NjksImV4cCI6MjA2MDE0ODQ2OX0.T3-cleAU5dDycOcwgKT9zxe1Pa5BD3OE4jRccMo7TW0`;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("failed to load supabase url and key");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
