ba9c8191a108f9827a35f2c15e2a36262c334d58
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-18 12:48:14 +0200

    Fixed

--- src/features/ShopDisplay/Sections/Products&Filters/Products/Products.tsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Products/Products.tsx
@@ -4,7 +4,7 @@ import ProductItem from "./ProductItem";
 // grid grid-cols-1 justify-self-center sm:grid-cols-2 md:grid-cols-3 lg:col-start-4 lg:col-end-14 lg:mt-0
 export default function Products() {
   const { products, isLoading, error } = useProducts();
-  if (true)
+  if (isLoading)
     return (
       <div className="col-span-full text-center lg:col-start-4 lg:col-end-13">
         <span className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-black border-b-transparent lg:h-16 lg:w-16"></span>
@@ -13,20 +13,20 @@ export default function Products() {
   if (error) {
     toast.error("Error! Please reload the page");
   }
-  // return (
-  //   <div className="col-span-full mt-8 grid grid-cols-1 gap-5 justify-self-center sm:grid-cols-2 md:grid-cols-3 lg:col-start-4 lg:col-end-13 lg:mt-0 lg:grid-cols-3 2xl:grid-cols-4">
-  //     {products?.map((product) => (
-  //       <ProductItem key={product.id} product={product} />
-  //     ))}
-  //     {/* <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem />
-  //     <ProductItem /> */}
-  //   </div>
-  // );
+  return (
+    <div className="col-span-full mt-8 grid grid-cols-1 gap-5 justify-self-center sm:grid-cols-2 md:grid-cols-3 lg:col-start-4 lg:col-end-13 lg:mt-0 lg:grid-cols-3 2xl:grid-cols-4">
+      {products?.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+      {/* <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem /> */}
+    </div>
+  );
 }
--- src/services/Supabase.ts
+++ src/services/Supabase.ts
@@ -1,15 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-<<<<<<< HEAD
-const supabaseUrl = "https://uaeozjttrvrpjlgyopnu.supabase.co";
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhZW96anR0cnZycGpsZ3lvcG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1NDQzMTgsImV4cCI6MjA0NTEyMDMxOH0.yyDHe018Kx7fLRtKnlEtpONkDo9Ou6VGfIRD6YSoHIQ`;
-=======
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("failed to load supabase url and key");
 }
 
->>>>>>> 0e75c49 (Removed env from commiting)
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
