05a2466f29f1034f8744b5a0c254436ab8c5dc0e
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-04-27 15:59:10 +0200

    Finished display part of product data from supabase

--- src/features/ShopDisplay/Sections/Products&Filters/Products/ProductItem.tsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Products/ProductItem.tsx
@@ -45,19 +45,13 @@ export default function ProductItem({ product }: Props) {
   } = product;
   return (
     <div className="shadow-my-shadow relative flex w-full cursor-pointer flex-col items-center justify-center gap-3 rounded-sm p-10">
-      <img
-        className="mb-5 w-[60%] rounded-sm"
-        src={product.image}
-        alt="zara perfume"
-      />
+      <img className="mb-5 w-[60%] rounded-sm" src={image} alt="zara perfume" />
       <p className="text-xl">
-        {product.Brand.name} {product.title}
+        {name} {title}
       </p>
-      <span className="text-main-color-shade/70">{product.Gender.gender}</span>
-      <span className="text-main-color-shade/70">
-        {product.Product_type.type}
-      </span>
-      <p>{product.price} RSD</p>
+      <span className="text-main-color-shade/70">{gender}</span>
+      <span className="text-main-color-shade/70">{type}</span>
+      <p>{price} RSD</p>
       <span className="text-main-color-tint absolute top-[10px] right-[15px] text-xl">
         {/* @ts-expect-error  Typescript ne propaznaje iconu kao validan jsx element*/}
         <ion-icon name="heart"></ion-icon>
