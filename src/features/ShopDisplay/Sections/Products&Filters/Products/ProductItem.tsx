332253d04900a84de0049c2ab4db816a320939bb
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-02 23:42:03 +0200

    Trying to poistion a discount banner

--- src/features/ShopDisplay/Sections/Products&Filters/Products/ProductItem.tsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Products/ProductItem.tsx
@@ -56,7 +56,7 @@ export default function ProductItem({ product }: Props) {
   return (
     <div className="shadow-my-shadow relative flex w-full cursor-pointer flex-col items-center justify-center gap-3 overflow-hidden rounded-sm p-10">
       {sale_discount && (
-        <div className="text-secondary-color absolute top-5 -right-7 w-[120px] rotate-45 bg-black text-center">
+        <div className="text-secondary-color absolute top-5 -right-7">
           {sale_discount}%
         </div>
       )}
@@ -66,10 +66,12 @@ export default function ProductItem({ product }: Props) {
       </p>
       <span className="text-main-color-shade/70">{gender}</span>
       <span className="text-main-color-shade/70">{type}</span>
-      <p>{price} RSD</p>
-      {sale_discount && (
-        <p className="text-main-color-shade/70">{discountedPrice} RSD</p>
+      {sale_discount ? (
+        <s className="text-main-color-shade/70">{price} RSD</s>
+      ) : (
+        <p>{price} RSD</p>
       )}
+      {sale_discount && <p>{discountedPrice} RSD</p>}
       <span className="text-main-color-tint absolute top-[10px] right-[15px] text-xl">
         {/* @ts-expect-error  Typescript ne propaznaje iconu kao validan jsx element*/}
         <ion-icon name="heart"></ion-icon>
