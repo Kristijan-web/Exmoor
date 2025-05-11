0cdc8996a28d7a3c0cd22af86b25217c87c7d66d
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-11 23:15:58 +0200

    problem with icon fixed

--- src/features/ShopDisplay/Sections/Products&Filters/Products/ProductItem.tsx
+++ src/features/ShopDisplay/Sections/Products&Filters/Products/ProductItem.tsx
@@ -72,6 +72,7 @@ export default function ProductItem({ product }: Props) {
       {sale_discount && <p>{discountedPrice} RSD</p>}
       <span className="text-main-color-tint absolute top-[10px] right-[15px] text-xl">
         {/* @ts-expect-error  Typescript ne propaznaje iconu kao validan jsx element*/}
+        <ion-icon name="heart"></ion-icon>
       </span>
     </div>
   );
