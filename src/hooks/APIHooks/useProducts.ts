80fdcea9712c9e69b43f73daf136125c47a789f8
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-14 23:43:31 +0200

    Problem with importing files

--- src/hooks/APIHooks/useProducts.ts
+++ src/hooks/APIHooks/useProducts.ts
@@ -1,6 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
-import getProducts from "../../services/Products/getProducts";
-
 export default function useProducts() {
   const {
     data: products,
