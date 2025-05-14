a797e91853678ba196dac2bcb8cd14687da931d7
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-14 23:43:48 +0200

    Problem with importing fixed

--- src/hooks/APIHooks/useProducts.ts
+++ src/hooks/APIHooks/useProducts.ts
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import getProducts from "../../services/Products/getProducts";
+
 export default function useProducts() {
   const {
     data: products,
