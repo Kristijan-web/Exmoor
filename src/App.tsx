f5f5e6380c87c33a2fb12b7f03333b09b2c5e224
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-16 23:06:36 +0200

    Problem with react query fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
