e3ce1c689bd41b5e95a98618e6bbb5956577d408
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-20 23:39:37 +0200

    problem fixed

--- src/services/Supabase.ts
+++ src/services/Supabase.ts
@@ -1,3 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("failed to load supabase url and key");
 }
