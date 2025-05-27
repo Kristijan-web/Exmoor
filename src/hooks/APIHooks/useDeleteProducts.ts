13e85aa5415b1616a1012987dd691e1f889a8622
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-05-27 22:12:29 +0200

    Problem with react query

--- src/hooks/APIHooks/useDeleteProducts.ts
+++ src/hooks/APIHooks/useDeleteProducts.ts
@@ -2,6 +2,10 @@ import { useMutation } from "@tanstack/react-query";
 import deleteProduct from "../../services/Products/deleteProduct";
 
 export default function useDeleteProducts() {
-  const { mutate, isPending } = useMutation({});
+  const { mutate, isPending } = useMutation({
+    mutationFn: () => deleteProduct(),
+    onSuccess: () => console.log("yey"),
+    onError: (err) => console.log(err),
+  });
   return { mutate, isPending };
 }
