5dc94d7b0347350501790c93dddd35acb6990772
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-06-15 21:32:01 +0200

    Problem fixed

--- src/features/ContactDisplay/Sections/Gadgets/Gadgets.tsx
+++ src/features/ContactDisplay/Sections/Gadgets/Gadgets.tsx
@@ -2,7 +2,14 @@ import { IoLocationOutline, IoMailOutline } from "react-icons/io5";
 
 export default function Gadgets() {
   return (
-
+    <section className="mb-24">
+      <div className="mx-auto grid max-w-3xl grid-cols-12 gap-6 px-7">
+        <div className="col-span-full flex flex-col items-center justify-center gap-1 lg:col-start-1 lg:col-end-7">
+          <span className="text-3xl">
+            <IoMailOutline />
+          </span>
+          <p className="text-xl">exmoorperfumes23@gmail.com</p>
+        </div>
 
         <div className="col-span-full flex flex-col items-center justify-center gap-1 lg:col-start-7 lg:col-end-13">
           <span className="text-3xl">
