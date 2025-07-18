36d294095d57ea895901d12119f3f0a983dd503b
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-07-18 16:25:15 +0200

    uninstalled eslint

--- src/features/SettingsDisplay/SettingsDisplay.tsx
+++ src/features/SettingsDisplay/SettingsDisplay.tsx
@@ -17,7 +17,16 @@ export default function SettingsDisplay() {
     [lessThan1024Width],
   );
 
-
+  useEffect(function showSettingsMenu() {
+    function handleClick(e: MouseEvent) {
+      const target = e.target as HTMLElement;
+      if (target?.getAttribute("href") === "/podesavanja") {
+        setshowSidebar(true);
+      }
+    }
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
 
   return (
     <div className="mx-auto grid max-w-6xl grid-cols-1 lg:grid-cols-[305px_1fr] lg:p-7">
