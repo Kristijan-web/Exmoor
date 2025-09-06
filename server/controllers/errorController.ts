bc8d1ad48d9ee04724637e6b0ab9547974a84197
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-09-06 19:28:03 +0200

    Expanding errorController

--- server/controllers/errorController.ts
+++ server/controllers/errorController.ts
@@ -1,13 +1,33 @@
 import { NextFunction, Request, Response } from "express";
 
-function sendProduction(error: Error, res: Response) {
-  // if(error.isOperational) {
-  //   res.status(error.statusCode).send({
-  //     status: error.status
-  //     message: error.message,
-  //     isOperationa: true
-  //   })
-  // }
+// Sta je problem?
+// - Error objekat na sebi nema property-e objekta AppError
+// Sta ja zelim?
+// - Da sendProduction uspesno salje poruku kada je AppError greska i kada nije (Error)
+// Zasto samo ne stavim da sendProduction ocekuje parametar tipa AppError?
+// -
+
+interface AppErrorType extends Error {
+  isOperational: boolean;
+  statusCode: number;
+  status: string;
+}
+
+function sendProduction(error: AppErrorType | Error, res: Response) {
+  // Od cega ts pokusava da me zastiti?
+
+  if ("IsOperational" in error) {
+    res.status(error.statusCode).send({
+      status: error.status,
+      message: error.message,
+      isOperational: true,
+    });
+  } else {
+    res.status(500).send({
+      status: "fail",
+      message: "Something went wrong",
+    });
+  }
 }
 
 function sendDevelopment(error: Error, res: Response) {
@@ -20,7 +40,7 @@ function sendDevelopment(error: Error, res: Response) {
 }
 
 const globalErrorMiddleware = function (
-  error: Error,
+  error: AppErrorType,
   req: Request,
   res: Response,
   next: NextFunction
@@ -28,6 +48,7 @@ const globalErrorMiddleware = function (
   if (process.env.NODE_ENV === "development") {
     sendDevelopment(error, res);
   } else {
+    // dozvolim da se prosledi i Error | AppError ako je podatak tipa Error onda ga castujem u AppError i dodam property-e
     sendProduction(error, res);
   }
 };
--- server/utills/catchAsync.ts
+++ server/utills/catchAsync.ts
@@ -1,9 +1,21 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
+import AppError from "./appError";
 
+type AppNext = (err?: AppError) => void;
+
+// Sta mi nije jasno?
+// Ako sam rekao da funkcija ocekuje parametar tipa AppNext koji je opcioni AppError parametar a ja mu prosledjujem NextFunction koji ima tip any i moze biti obican string 'eeeee' to odmah znaci da ne ispunjavam ono sto prosledjena funkcija ocekuje, zasto onda ts ne prijavljuje error kod fn(req,res,next)
+
+// - Proveri pretpostovaku, da kada se izvrava anonimna funkcija i poziva fn(req,res,next) da next ocekuje AppNext =====> DA
+// - Proveri pretpostavku, da kada imam specifican tip parametra da onda ne moze da se prosledi any, npr:
+// type User = {name: string, email: string}
+// const getUser = function(user: User) {}   getUser('yo')
+
+// RESENJE DONEKLE: TYPESCRIPT TYPES SE PONASAJU DRUGACIJE KADA SU U PITANJU FUNKCIJE, NE RADI OCEKIVANO KAO SA OBJECT/VALUES
 const catchAsync = function (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>
+  fn: (req: Request, res: Response, next: AppNext) => Promise<void>
 ) {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: AppNext) => {
     fn(req, res, next).catch((err) => next(err));
   };
 };
