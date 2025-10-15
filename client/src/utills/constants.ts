d8529ad2c3afded15ec31fe5ac9bd096e8959d03
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-10-15 03:24:31 +0200

    Testing jenkins pipeline trigger on git push

--- client/src/utills/constants.ts
+++ client/src/utills/constants.ts
@@ -1,4 +1,4 @@
-const localhost_URL = "http://127.0.0.1:3000";
-// const production_URL = "https://exmoor.onrender.com"
+// const localhost_URL = "http://127.0.0.1:3000";
+// // const production_URL = "https://exmoor.onrender.com"
 
-export const API_URL = `${localhost_URL}`;
+export const API_URL = import.meta.env.VITE_API_URL;
