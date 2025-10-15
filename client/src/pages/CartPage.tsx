f6b96f9b164df327aab032b97d3d73eb05f855ef
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-10-15 02:26:17 +0200

    testing jenkins pipeline

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -1,6 +1,8 @@
 import CartDisplay from "../features/CartDisplay/CartDisplay";
 
-console.log("test");
+console.log("Testing");
+console.log("Testing");
+console.log("Testing");
 export default function CartPage() {
   return <CartDisplay />;
 }
