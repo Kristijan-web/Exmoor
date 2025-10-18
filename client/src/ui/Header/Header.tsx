d775c5015e1d2f01e90c990622232d7ded4bf47e
Kristijan Stojanovic <kristijanstojanovic11@gmail.com>
2025-10-18 22:17:27 +0200

    Made some changes to header

--- client/src/ui/Header/Header.tsx
+++ client/src/ui/Header/Header.tsx
@@ -78,9 +78,9 @@ export default function Header() {
           <li>
             <NavLink to="/">Početna</NavLink>
           </li>
-          {/* <li>
-            <NavLink to="/shop">Shop</NavLink>
-          </li> */}
+          <li>
+            <NavLink to="/shop">EEEE</NavLink>
+          </li>
           <li>
             <NavLink to="/kontakt">Kontakt</NavLink>
           </li>
