eeb6a3126995c80abd1aa9b0ad8c31667664a9fe
Kristijan <kristijanstojanovic11@gmail.com>
2025-11-22 00:44:10 +0100

    Finished html/css for addProduct panel in admin page

--- client/src/features/AdminDisplay/AdminPanels/ProductsPanel/AddProduct.tsx
+++ client/src/features/AdminDisplay/AdminPanels/ProductsPanel/AddProduct.tsx
@@ -1,19 +1,43 @@
-// import { useForm } from "react-hook-form";
-
+import { useForm } from "react-hook-form";
 import { useState } from "react";
 
+type Sale = {
+  discount: number;
+  sale_start: string;
+  sale_end: string;
+  sold: number;
+};
+
+type FormData = {
+  title: string;
+  brand: string;
+  gender: string;
+  water: string;
+  price: number;
+  quantity: number;
+  image: FileList;
+  sale: Sale | null;
+};
+
 export default function AddProduct() {
-  //   const { formData } = useForm();
-  const [enabled, setEnabled] = useState(false);
+  const { register, formState, handleSubmit } = useForm<FormData>();
+  const { errors } = formState;
+  const [showSale, setshowSale] = useState(false);
+
+  function onSuccess(data: FormData) {
+    console.log("Evo podataka iz forme", data);
+  }
+
+  // Fix bitan
+  // - Kada sale nije oznacen onda polja ispod njega ne treba da budu obavezna
+
   return (
     <section className="col-start-1 col-end-3 flex h-full items-center justify-center p-4 lg:col-start-2 lg:col-end-3">
       <div className="flex flex-col gap-6 rounded-lg border-1 border-gray-400 p-6">
         <h2 className="text-3xl font-medium">Dodaj novi proizvod</h2>
         <form
           className="flex flex-col items-start gap-4"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
+          onSubmit={handleSubmit(onSuccess)}
         >
           {/* Pocetak */}
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
@@ -26,7 +50,11 @@ export default function AddProduct() {
                 type="text"
                 placeholder="npr. Icy, Cool, Smooth"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("title", { required: "Ovo polje je obavezno" })}
               />
+              {errors?.title?.message && (
+                <p className="text-red-500">{errors.title.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="naziv" className="font-semibold">
@@ -37,29 +65,50 @@ export default function AddProduct() {
                 type="text"
                 placeholder="npr. Zara, Davidoff"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("brand", { required: "Ovo polje je obavezno" })}
               />
+              {errors?.brand?.message && (
+                <p className="text-red-500">{errors.brand.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="gender">Pol *</label>
               <select
                 id="gender"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("gender", {
+                  required: "Ovo polje je obavezno",
+                  validate: (value) =>
+                    value === "Izaberi..." || "Ovo polje je obavezno",
+                })}
               >
                 <option value="default">Izaberi...</option>
                 <option value="muski">Muški</option>
                 <option>Ženski</option>
               </select>
+              {errors?.gender?.message && (
+                <p className="text-red-500">{errors.gender.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="water_type">Vrsta vode *</label>
               <select
                 id="water_type"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("water", {
+                  required: "Ovo polje je obavezno",
+                  validate: (value) =>
+                    value === "Izaberi..." || "Ovo polje je obavezno ",
+                })}
               >
+                <option>Izaberi...</option>
                 <option value="Parfem">Parfemska voda</option>
                 <option value="Toaletna">Toaletna voda</option>
                 <option value="Kolonjska">Kolonjska voda</option>
               </select>
+              {errors?.water?.message && (
+                <p className="text-red-500">{errors.water.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="price">Cena *</label>
@@ -68,7 +117,11 @@ export default function AddProduct() {
                 id="price"
                 type="number"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("price", { required: "Ovo polje je obavezno" })}
               />
+              {errors?.price?.message && (
+                <p className="text-red-500">{errors.price.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="quantity">Kolicina</label>
@@ -77,14 +130,22 @@ export default function AddProduct() {
                 id="quantity"
                 type="number"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("quantity", { required: "Ovo polje je obavezno" })}
               />
+              {errors?.quantity?.message && (
+                <p className="text-red-500">{errors.quantity.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label>Slika *</label>
               <input
                 type="file"
                 className="rounded-md border border-gray-300 px-3 py-2"
+                {...register("image", { required: "Ovo polje je obavezno" })}
               />
+              {errors?.image?.message && (
+                <p className="text-red-500">{errors.image.message}</p>
+              )}
             </div>
           </div>
 
@@ -93,24 +154,24 @@ export default function AddProduct() {
             <p>Proizvod je na akciji?</p>
             <div className="flex items-center justify-start gap-3">
               <button
-                onClick={() => setEnabled(!enabled)}
+                onClick={() => setshowSale(!showSale)}
                 className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors duration-300 ${
-                  enabled ? "bg-green-500" : "bg-gray-600"
+                  showSale ? "bg-green-500" : "bg-gray-600"
                 }`}
               >
                 <span
                   className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-300 ${
-                    enabled ? "translate-x-6" : "translate-x-1"
+                    showSale ? "translate-x-6" : "translate-x-1"
                   }`}
                 />
               </button>
-              <span>{enabled ? "Da" : "Ne"}</span>
+              <span>{showSale ? "Da" : "Ne"}</span>
             </div>
           </div>
 
           {/* Ispod je div koji je grid i sadrzi 4 grid-item-a */}
           <div
-            className={`grid grid-cols-1 gap-5 lg:grid-cols-2 lg:grid-rows-2 ${enabled ? "grid" : "hidden"}`}
+            className={`items-senter grid w-full grid-cols-1 justify-center gap-5 lg:grid-cols-3 lg:grid-rows-1 ${showSale ? "grid" : "hidden"}`}
           >
             <div className="flex flex-col items-start gap-2">
               <label htmlFor="popust" className="font-semibold">
@@ -120,8 +181,14 @@ export default function AddProduct() {
                 placeholder="npr. 10"
                 id="popust"
                 type="text"
-                className="rounded-md border border-gray-300 px-3 py-2"
+                className="w-full rounded-md border border-gray-300 px-3 py-2"
+                {...register("sale.discount", {
+                  required: "Ovo polje je obavezno",
+                })}
               />
+              {errors?.sale?.discount?.message && (
+                <p className="text-red-500">{errors.sale.discount.message}</p>
+              )}
             </div>
             <div className="flex flex-col items-start gap-2">
               <label htmlFor="pocetak_akcije" className="font-semibold">
@@ -130,8 +197,14 @@ export default function AddProduct() {
               <input
                 id="pocetak_akcije"
                 type="date"
-                className="rounded-md border border-gray-300 px-3 py-2"
+                className="w-full rounded-md border border-gray-300 px-3 py-2"
+                {...register("sale.sale_start", {
+                  required: "Ovo polje je obavezno",
+                })}
               />
+              {errors?.sale?.sale_start?.message && (
+                <p className="text-red-500">{errors.sale.sale_start.message}</p>
+              )}
             </div>
             <div className="flex flex-col items-start gap-2">
               <label htmlFor="naziv" className="font-semibold">
@@ -140,8 +213,14 @@ export default function AddProduct() {
               <input
                 id="kraj_akcije"
                 type="date"
-                className="rounded-md border border-gray-300 px-3 py-2"
+                className="w-full rounded-md border border-gray-300 px-3 py-2"
+                {...register("sale.sale_end", {
+                  required: "Ovo polje je obavezno",
+                })}
               />
+              {errors?.sale?.sale_end?.message && (
+                <p className="text-red-500">{errors.sale.sale_end.message}</p>
+              )}
             </div>
           </div>
           <div>
