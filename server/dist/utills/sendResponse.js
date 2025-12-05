8cc88093513c0af0a62d9bab8273047b69f9b8c7
Kristijan <kristijanstojanovic11@gmail.com>
2025-12-05 02:30:17 +0100

    Stop tracking dist

--- server/dist/utills/sendResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sendResponse(res, data) {
-    res.status(200).json({
-        message: "success",
-        data,
-    });
-}
-exports.default = sendResponse;
