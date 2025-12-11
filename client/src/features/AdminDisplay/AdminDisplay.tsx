7ca70d95394e26f859e4b92892632997f6734bbb
Kristijan-web <kristijanstojanovic11@gmail.com>
2025-12-11 23:47:35 +0100

    Setting up environment on new laptop

--- client/src/features/AdminDisplay/AdminDisplay.tsx
+++ client/src/features/AdminDisplay/AdminDisplay.tsx
@@ -7,7 +7,6 @@ import { useMediaQuery } from "react-responsive";
 export default function AdminDisplay() {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
   const [toggleSidebar, setToggleSidebar] = useState<boolean>(true);
-
   return (
     <>
       <section className="grid grid-cols-[300px_1fr] grid-rows-[100px_1fr]">
